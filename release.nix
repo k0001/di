c9367707df1158b10836b3088764e6982deea716
Renzo Carbonara <gnuk0001@gmail.com>
2017-06-30 15:47:55 +0200

    travis

--- release.nix
+++ release.nix
@@ -1,19 +1,12 @@
 { nixpkgsBootstrap ? <nixpkgs>
-, nixpkgs ? (import nixpkgsBootstrap {}).fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "b12aacc7c18fb3f29febc842aaa3d0c0e5622546"; # release-17.03
-    sha256 = "1qklmrvfnlk1r6rqxpddllfkv3pihfms370qfsznm959i7hxcv2v"; }
+, nixpkgs ? builtins.fetchTarball
+    https://github.com/NixOS/nixpkgs-channels/archive/nixos-17.03.tar.gz
 }:
-
 let
-
 pkgs = import nixpkgs {};
-
 hsPackageSetConfig = self: super: {
   di = self.callPackage (import ./pkg.nix) {};
 };
-
 ghc802 = pkgs.haskell.packages.ghc802.override {
   packageSetConfig = hsPackageSetConfig;
 };
