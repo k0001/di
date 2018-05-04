9a44f1b5a5a7582647293cc748c8ef564ada1188
Renzo Carbonara <gnuk0001@gmail.com>
2018-05-04 21:03:10 +0200

    Moved 'di' into its own subfolder. Moved Nix stuff around. Require Nix 2

--- release.nix
+++ release.nix
@@ -1,13 +1,12 @@
-{ nixpkgsBootstrap ? <nixpkgs>
-, nixpkgs ? builtins.fetchTarball channel:nixos-18.03
+{ nixpkgs ? builtins.fetchTarball channel:nixos-18.03
 }:
-let
-pkgs = import nixpkgs {};
-hsPackageSetConfig = self: super: {
-  di = self.callPackage (import ./pkg.nix) {};
-};
-ghc822 = pkgs.haskell.packages.ghc822.override {
-  packageSetConfig = hsPackageSetConfig;
-};
 
-in { inherit (ghc822) di; }
+let
+  pkgs = import nixpkgs {};
+  ghc841 = pkgs.haskell.packages.ghc841.override {
+    packageSetConfig = import ./hs-overlay.nix;
+  };
+
+in {
+  inherit (ghc841) di;
+}
